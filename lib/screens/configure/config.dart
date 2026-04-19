5d2eb2b7cec4db10def609e1c45c3882259e5454
Ratnesh Pandey <156459822+Ratnesh-pandey-01@users.noreply.github.com>
2026-04-19 13:00:19 +0530

    Removed config file from repo

--- lib/screens/configure/config.dart
+++
@@ -1,7 +0,0 @@
-class AppConfig {
-  static const cloudinaryVideoUrl =
-      "https://api.cloudinary.com/v1_1/dev6zewn8/video/upload";
-  static const cloudinaryImageUrl =
-      "https://api.cloudinary.com/v1_1/dev6zewn8/image/upload";
-  static const fcmKey = "";
-}
